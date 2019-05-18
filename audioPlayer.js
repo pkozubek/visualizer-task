eb7be14d6d8c31522906988b6463794ccb5b6df6
kozi <pkozubek92@gmail.com>
2019-05-18 12:47:50 +0200

    init

---
+++ audioPlayer.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    var canvas = document.getElementById('visualizer');
+
+    document.getElementById('play').addEventListener('click',analyzeMusic);
+
+    function analyzeMusic(){
+
+        var audio = document.getElementById('song');
+        var audioCtx = new AudioContext();
+        var audioSrc = audioCtx.createMediaElementSource(audio);
+        var analyser = audioCtx.createAnalyser();
+
+        audioSrc.connect(analyser);
+        analyser.connect(audioCtx.destination);
+
+        audioSrc.connect(analyser);
+        var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    
+        function renderFrame() {
+            requestAnimationFrame(renderFrame);
+            analyser.getByteFrequencyData(frequencyData);
+        }
+
+        audio.play();
+        renderFrame();
+
+        
+    }
+}
